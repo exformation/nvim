e30499e831412e4f62d7a574438b3aaf0556fe0e
exform <mmillwood256@gmail.com>
2023-03-19 17:09:40 -0500

    boop

--- init.lua
+++ init.lua
@@ -133,49 +133,28 @@ require('lazy').setup({
   { import = 'custom.plugins' },
 }, {})
 
--- [[ Setting options ]]
 -- See `:help vim.o`
+o = vim.o
+wo = vim.wo
 
--- Set highlight on search
-vim.o.hlsearch = false
-
--- Make line numbers default
-vim.wo.number = true
-
--- Enable mouse mode
-vim.o.mouse = 'a'
-
--- Sync clipboard between OS and Neovim.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
-
--- Enable break indent
-vim.o.breakindent = true
-
--- Save undo history
-vim.o.undofile = true
-
--- Case insensitive searching UNLESS /C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
--- Keep signcolumn on by default
+o.hlsearch = false
+o.scrolloff = 999
+wo.number = true
+o.mouse = 'a'
+o.clipboard = 'unnamedplus'
+o.breakindent = true
+o.undofile = true
+o.ignorecase = true
+o.smartcase = true
+o.relativenumber = true
 vim.wo.signcolumn = 'yes'
-
--- Decrease update time
--- vim.o.updatetime = 250
--- vim.o.timeout = true
--- vim.o.timeoutlen = 300
-
--- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'
-
--- NOTE: You should make sure your terminal supports this
-vim.o.termguicolors = true
+-- o.updatetime = 250
+-- o.timeout = true
+-- o.timeoutlen = 300
+o.completeopt = 'menuone,noselect'
+o.termguicolors = true
 
 -- [[ Basic Keymaps ]]
-
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
@@ -227,6 +206,8 @@ vim.keymap.set('n', '<leader>/', function()
     previewer = false,
   })
 end, { desc = '[/] Fuzzily search in current buffer' })
+
+-- vim.keymap.set('n', '<leader>q', ":wq", { desc = '[Q]uit' })
 
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
