0e28a9419a76cf866713ecfd8d5fdc5cfb6c1b72
exform <mmillwood256@gmail.com>
2023-06-06 12:59:51 -0500

    boop

--- after/plugin/misc.lua
+++ after/plugin/misc.lua
@@ -5,5 +5,8 @@ require('better_escape').setup {
   mapping = { 'jk' },
   timeout = 100,
 }
+require("chatgpt").setup {
+  api_key_cmd = "gpg --decrypt ~/OPENAI.txt.gpg 2>/dev/null"
+}
 require('onedark').load()
 require('persistence').load()
