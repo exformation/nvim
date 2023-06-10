c34741edc04c84ecd0bf51f041be28be4eb220d0
exform <mmillwood256@gmail.com>
2023-06-10 15:02:17 -0500

    boop

--- after/plugin/misc.lua
+++ after/plugin/misc.lua
@@ -40,6 +40,17 @@ require('hop').setup {
   keys = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~",
   -- keys = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
 }
+require('nvim-treesitter.configs').setup {
+  rainbow = {
+    enable = true,
+    -- list of languages you want to disable the plugin for
+    disable = { 'jsx', 'cpp' },
+    -- Which query to use for finding delimiters
+    query = 'rainbow-parens',
+    -- Highlight the entire buffer all at once
+    strategy = require('ts-rainbow').strategy.global,
+  }
+}
 
 require('onedark').load()
 require('persistence').load()
