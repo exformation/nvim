8e5542b4b8f27e1945978fe2d08ebf441e58565e
exform <mmillwood256@gmail.com>
2023-06-13 12:42:20 -0500

    boop

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -9,9 +9,9 @@ O.expandtab = true
 O.hlsearch = false
 O.ignorecase = true
 -- O.nohlsearch = true
-O.number = true
-O.relativenumber = true
-O.scrolloff = 999
+O.number = false
+-- O.relativenumber = true
+-- O.scrolloff = 999
 O.shiftwidth = 2
 O.signcolumn = 'yes'
 O.smartcase = true
@@ -28,3 +28,13 @@ O.foldmethod = 'expr'
 O.foldexpr = 'nvim_treesitter#foldexpr()'
 -- O.nofoldenable = true
 O.foldlevel = 1
+
+vim.api.nvim_create_augroup("CenterCursor", { clear = true })
+vim.api.nvim_create_autocmd("CursorMoved", {
+  group = "CenterCursor",
+  pattern = "*",
+  command = "normal! zz",
+})
+
+-- vim.api.nvim_exec("autocmd VimEnter * mapclear", false)
+-- vim.api.nvim_exec("autocmd VimEnter * mapclear!", false)
