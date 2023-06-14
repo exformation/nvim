2390988affcb4e72ad0cbcb42900e62b11eea53d
exform <mmillwood256@gmail.com>
2023-06-13 23:35:02 -0500

    boop

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -3,6 +3,12 @@
 -- -- See `:help telescope.builtin`
 -- -- TODO: explore telescope extensions (file browsers, gh, etc.)
 require('telescope').setup {
+  defaults = {
+    file_ignore_patterns = {
+      ".*.lock",
+      ".*hardware%-configuration.nix"
+    }
+  }
   --   defaults = {
   --     prompt_prefix = 'test$ ',
   --     mappings = {
@@ -33,4 +39,4 @@ require('telescope').setup {
 }
 -- require('telescope').load_extension('fzf')
 -- require('telescope').load_extension('projects')
-require("telescope").load_extension("flutter")
+-- require("telescope").load_extension("flutter")
