3de05a354a7d5fdaea7e3b2d97c20feaa0784de8
exform <mmillwood256@gmail.com>
2024-02-12 10:35:27 -0600

    boop

--- init.lua
+++ init.lua
@@ -14,7 +14,28 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 O = vim.o
 O.clipboard = 'unnamedplus'
+O.expandtab = true
+O.ignorecase = true
+O.number = false
+O.relativenumber = false
+O.shiftwidth = 2
+O.smartcase = true
+O.tabstop = 2
+O.termguicolors = true
+O.timeout = true
+O.timeoutlen = 0
+O.undofile = true
+O.updatetime = 0
 
+vim.api.nvim_create_augroup("CenterCursor", { clear = true })
+vim.api.nvim_create_autocmd("CursorMoved", {
+  group = "CenterCursor",
+  pattern = "*",
+  command = "normal! zz",
+})
+
+vim.cmd([[autocmd FocusLost * :wa]])
+vim.cmd([[autocmd BufLeave * :wa]])
 
 require("lazy").setup("plugins")
 
