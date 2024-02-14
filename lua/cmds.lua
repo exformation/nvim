4a87d9b0c314349d0d9993960e5934c6d3167687
exform <mmillwood256@gmail.com>
2024-02-13 21:59:15 -0600

    boop

--- lua/cmds.lua
+++ lua/cmds.lua
@@ -10,6 +10,9 @@ vim.api.nvim_create_autocmd({ 'FocusLost', 'BufLeave', 'VimLeavePre' }, {
   callback = function()
     if vim.lsp.buf.server_ready() then
       vim.lsp.buf.format { async = false }
+      if vim.bo.modifiable then
+        vim.cmd 'write'
+      end
     end
   end,
 })
