d578672ef0ad9e1d450e9aa3e64d85e539b364e4
exform <mmillwood256@gmail.com>
2024-02-22 17:58:13 -0600

    boop

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -14,21 +14,12 @@ return {
     local dap = require 'dap'
     local widgets = require 'dap.ui.widgets'
 
+    -- TODO: this should probably go somewhere else (require(myterminal))
     local Terminal = require('toggleterm.terminal').Terminal
     local lazygit = Terminal:new {
       cmd = 'lazygit',
       dir = 'git_dir',
       direction = 'float',
-      float_opts = {
-        border = 'double',
-      },
-      on_open = function(term)
-        vim.cmd 'startinsert!'
-        vim.api.nvim_buf_set_keymap(term.bufnr, 'n', 'q', '<cmd>close<CR>', { noremap = true, silent = true })
-      end,
-      on_close = function(_)
-        vim.cmd 'startinsert!'
-      end,
     }
 
     wk.register {
