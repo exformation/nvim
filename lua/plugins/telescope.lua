ea4c65feec8b405cd9e706a70290f7022c2ba10a
exform <mmillwood256@gmail.com>
2024-03-05 20:38:24 -0600

    boop

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -15,6 +15,33 @@ return {
         },
       }
       require('telescope').load_extension 'ui-select'
+
+      local as = require 'telescope.actions.state'
+      local actions = require 'telescope.actions'
+
+      -- https://github.com/nvim-telescope/telescope.nvim/blob/master/lua/telescope/actions/init.lua#L941
+      local get_entries = function(bufnr)
+        local manager = as.get_current_picker(bufnr).manager
+        local entries = {}
+        for entry in manager:iter() do
+          table.insert(entries, entry)
+        end
+        return entries
+      end
+
+      require('telescope').setup {
+        defaults = {
+          mappings = {
+            i = {
+              ['<C-j>'] = function(bufnr)
+                local entries = get_entries(bufnr)
+                print(vim.inspect(entries))
+                actions.close(bufnr)
+              end,
+            },
+          },
+        },
+      }
     end,
   },
 }
