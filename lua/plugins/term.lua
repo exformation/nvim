f9846864a4e1c4347c23ebe6af67d080e8993f83
exform <mmillwood256@gmail.com>
2024-03-21 21:39:48 -0500

    boop

--- lua/plugins/term.lua
+++ lua/plugins/term.lua
@@ -2,6 +2,7 @@ return {
   'akinsho/toggleterm.nvim',
   config = function()
     require('toggleterm').setup {
+      shade_terminals = false,
       direction = 'tab',
       open_mapping = [[<c-k>]],
     }
--- lua/plugins/ts.lua
+++ lua/plugins/ts.lua
@@ -1,5 +1,6 @@
--- TODO: you removed paragraph dumby
 -- JK should work in visual
+-- repeat should still work for stuff like ft
+-- get indent to be defined here
 return {
   'nvim-treesitter/nvim-treesitter',
   dependencies = {
@@ -89,9 +90,10 @@ return {
 }
 -- plugin that shows a list of all text objects that are valid for the file and highlights them
 --[
+-- delete/change/yank/visual/goto/mark/swap/search
 -- inner/around/next/prev/first/last (repeatable)(surroundable)
 -- select/swap/move
--- cci -> change condition of if statement? 
+-- cci -> change condition of if statement?
 -- cbi -> change body of if statement?
 -- hjkl
 -- word cases
@@ -122,7 +124,67 @@ return {
 -- diagnostic/error/warning/hint/info
 -- indent/blanklines
 -- universe (whole file)
+-- url, owner/repo, email, phone (might as well make super niche shit selectable via telescope and then it's repeatable)
+-- qflist item? tons of things could be part of a list that you can nav, but should the list item be an object?
+-- search match: you can unmap nN and do j/k{match}.... to move between matches
+-- start of line, end of line, start of file, end of file ... even these are objects?
+-- breakpoints ...
+-- folds
+-- history
+-- jump list
+-- source tag
+-- next/prev buffer/window/tab
+-- some of these make sense for next/prev but as objects?
+-- also the way in which you typically like to go to the *previous* buffer is not the same
+-- TODO/FIXME/NOTE etc
+-- staged files
 --
--- easy way to delete all matching? 
+-- easy way to delete all matching?
+-- you should really generalize and just rely on repeating
+-- like, move hjklm etc to text objects, do things like {next}w . . . to advance words
+-- tons of things could be
+--
+-- s is surround so maybe w could be swap
+--
+-- motions are just text objects being fed into next/prev automatically
+--
+-- {verb}{modifier}{object}
+-- if I get rid of the default motions in favor of something more general, then modifiers are super empty
+-- you get around/inner for most things, left/right for some things
+-- maybe you could do something like cji" to change inner of the next pair of quotes (or cki" etc)
+--
+-- repeat-forward/repeat-backward
+--
+-- okay so you could do crazy shit like wkrai( to swap the right side of the assignment above you with i(
+--
+-- gw (goto-word) could put a hint on all words?
+--
+-- god you could get insane with this, each operator could be an object, so doing things like cl+ would change the left side of the + operator
+-- this means you should probably have a more generic operator object that takes the operator as an argument
+-- clo/<cr>
+-- so if way below you there's a something/something operation, you can immediately change the numerator
+-- just doing clo<cr> will change the left side of the operator you're in or nearest right/down
+--
+-- the search verb can accept an object so you can search all those objects /im -> search the contents  of all methods in the buffer
+-- should there be a modifier that let's you do these {verb}{modifier}{object} actions across multiple files?
+-- */ra search all files for right side of assignments ... idk if ts keeps all files open like that - probably not?
+--
+-- because so many things will not have around/inner, should you swap the order?
+-- {verb}{object}{modifier}
+-- d$ short circuits because it has no modifiers, w has a modifier so dwa wouldn't
+-- otherwise if you want things like d$ to stay concise you'll have to polute all the modifiers with objects
+-- local function swapCommandOrder()
+--     local char1 = vim.fn.nr2char(vim.fn.getchar())
+--     local char2 = vim.fn.nr2char(vim.fn.getchar())
+--     return char2 .. char1
+-- end
+-- vim.api.nvim_set_keymap('n', 'c', '"c" .. swapCommandOrder()', { noremap = true, expr = true })
+-- vim.api.nvim_set_keymap('n', 'd', '"d" .. swapCommandOrder()', { noremap = true, expr = true })
+-- you need to see how plugins do it -> gcc is in the keymaps but gciw isn't
+--
+-- commenting something is both an object and a verb
+-- gBkm -> block comment previous method
+-- j/  -> next comment
+
 --
 --]
